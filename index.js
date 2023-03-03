c428e9b57f2a8cb195bb076c2739714b258c2707
Michael Bisgaard Olesen <mo@concordium.com>
2023-03-03 10:18:28 +0100

    Implement the action (#8)
    The only input of the action is the string `tag` which is expected to match the format `[refs/tags/]<project-name>/<project-version>`.
    
    The action parses the tag into these components and outputs `<project-name>` and `<project-version>` as fields of the same names.
    
    See the `test.yml` workflow for an example of how to access these outputs in the same job or a dependent one.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const {parseTag} = require("./parse");
 
-
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
+try {
+    const tag = core.getInput('tag');
+    const {projectName, projectVersion} = parseTag(tag);
+    core.setOutput('project-name', projectName);
+    core.setOutput('project-version', projectVersion);
+} catch (error) {
     core.setFailed(error.message);
-  }
 }
-
-run();
